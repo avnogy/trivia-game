f12cf8b8eff1aa78b932fa61e9de066cd2f4cf84
Amir <amirhaimmizrahi.private@gmail.com>
2022-04-27 16:51:24 +0300

    Deleting old files

--- Trivia Project/Backend/src/Json.cpp
+++
@@ -1,9 +0,0 @@
-#include "json.h"
-
-JsonRequestPacketDeserializer::JsonRequestPacketDeserializer() {}
-
-JsonRequestPacketDeserializer::~JsonRequestPacketDeserializer() {}
-
-JsonResponsePacketSerializer::JsonResponsePacketSerializer() {}
-
-JsonResponsePacketSerializer::~JsonResponsePacketSerializer() {}
