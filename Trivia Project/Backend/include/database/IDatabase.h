28507f89bfcc80dbc18c205abb7274d243e2065a
Avner Mindelis <avner.mindelis@gmail.com>
2022-05-02 18:43:55 +0300

    implemented IDatabase and deleted cpp file

--- Trivia Project/Backend/include/database/IDatabase.h
+++ Trivia Project/Backend/include/database/IDatabase.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <string>
+
+class IDatabase
+{
+protected:
+	virtual bool doesUserExist(std::string username) = 0;
+	virtual bool doesPasswordMatch(std::string username, std::string password) = 0;
+	virtual void addNewUser(std::string username, std::string password, std::string email) = 0;
+};
