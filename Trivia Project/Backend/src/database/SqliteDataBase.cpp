9430cecabf1e8cce9f7925c3de340863d6ff6bf5
Avner Mindelis <avner.mindelis@gmail.com>
2022-05-02 20:19:14 +0300

    implemented addNewUser

--- Trivia Project/Backend/src/database/SqliteDataBase.cpp
+++ Trivia Project/Backend/src/database/SqliteDataBase.cpp
@@ -4,10 +4,10 @@
 SqliteDataBase::SqliteDataBase(std::string databasePath) :filePath(databasePath)
 {
 	//checking if can access file
-	int doesFileExist = !_access(filePath.c_str(), ACCESS_CODE); 
+	int doesFileExist = !_access(filePath.c_str(), ACCESS_CODE);
 
 	//attempting database open 
-	int res = sqlite3_open(filePath.c_str(), &db); 
+	int res = sqlite3_open(filePath.c_str(), &db);
 
 	if (res != SQLITE_OK)
 	{
@@ -43,7 +43,6 @@ bool SqliteDataBase::doesUserExist(std::string username)
 {
 	struct sqlite3_stmt* selectstmt;
 	int result = sqlite3_prepare_v2(db, ("SELECT * FROM USERS WHERE USERNAME = " + username + ";").c_str(), -1, &selectstmt, NULL);
-
 	if (result == SQLITE_OK)
 	{
 		bool found = sqlite3_step(selectstmt) == SQLITE_ROW; //if row was returned the user exists
@@ -55,11 +54,16 @@ bool SqliteDataBase::doesUserExist(std::string username)
 bool SqliteDataBase::doesPasswordMatch(std::string username, std::string password)
 {
 	struct sqlite3_stmt* selectstmt;
-	int result = sqlite3_prepare_v2(db, ("SELECT * FROM USERS WHERE USERNAME = " + username + " AND PASSWORD = "+ password +"; ").c_str(), -1, &selectstmt, NULL); 
+	int result = sqlite3_prepare_v2(db, ("SELECT * FROM USERS WHERE USERNAME = " + username + " AND PASSWORD = " + password + "; ").c_str(), -1, &selectstmt, NULL);
 	if (result == SQLITE_OK)
 	{
 		bool match = sqlite3_step(selectstmt) == SQLITE_ROW;	//if row was returned the passwoed matches
 		sqlite3_finalize(selectstmt);
 		return match;
 	}
+}
+
+void SqliteDataBase::addNewUser(std::string username, std::string password, std::string email)
+{
+	sqlexec("INSERT INTO USERS (NAME,PASSWORD,EMAIL) VALUES (" + username + "," + password + "," + email + ");");
 }
