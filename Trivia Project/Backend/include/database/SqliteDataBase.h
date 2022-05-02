0cf3161f21441283dc88785b7aabaefd1cc0756f
Avner Mindelis <avner.mindelis@gmail.com>
2022-05-02 18:59:48 +0300

    added sqlite fields and functions decleration

--- Trivia Project/Backend/include/database/SqliteDataBase.h
+++ Trivia Project/Backend/include/database/SqliteDataBase.h
@@ -1,2 +1,22 @@
 #pragma once 
+#include "database/IDatabase.h"
 #include "sqlite3.h"
+#include <string>
+#include <iostream>
+#include <io.h>
+
+class SqliteDataBase : public IDatabase
+{
+public:
+	SqliteDataBase();
+	~SqliteDataBase();
+	
+	void sqlexec(std::string msg);
+
+	bool doesUserExist(std::string username) override;
+	bool doesPasswordMatch(std::string username, std::string password) override;
+	void addNewUser(std::string username, std::string password, std::string email) override;
+private:
+	sqlite3* db;
+	std::string filePath;
+};
