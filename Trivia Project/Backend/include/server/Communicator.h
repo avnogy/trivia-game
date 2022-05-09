f9122d7cb5bcb3650e591c7d85f8e361cf99adf7
Amir Haim Mizrahi <amirhaimmizrahi.private@gmail.com>
2022-05-09 17:47:34 +0300

    Updating Communicator

--- Trivia Project/Backend/include/server/Communicator.h
+++ Trivia Project/Backend/include/server/Communicator.h
@@ -7,6 +7,7 @@
 #include "json/JsonRequestPacketDeserializer.h"
 #include "json/JsonRequestPacketSerializer.h"
 #include "handlers/LoginRequestHandler.h"
+#include "handlers/RequestHandlerFactory.h"
 
 #define PORT 4206
 
@@ -15,11 +16,14 @@ class Communicator
 private:
 	Socket m_serverSocket;
 	std::map<Socket*, IRequestHandler*> m_clients;
+	RequestHandlerFactory& m_handlerFactory;
 
 	RequestInfo recvRequest(Socket& socket);
 
 	void bindAndListen();
 	void handleNewClient(Socket** socket);
 public:
+	Communicator(RequestHandlerFactory& handlerFactory);
+
 	void startHandleRequest();
 };
