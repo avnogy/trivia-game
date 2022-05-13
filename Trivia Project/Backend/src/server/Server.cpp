f7e1eeb31420ed54e3ca7eb2be3f09acbea272c7
Amir Haim Mizrahi <amirhaimmizrahi.private@gmail.com>
2022-05-13 16:01:15 +0000

    Server can print login and signup messages from users. Also is can add login and signup requests to database

--- Trivia Project/Backend/src/server/Server.cpp
+++ Trivia Project/Backend/src/server/Server.cpp
@@ -22,3 +22,13 @@ void Server::run()
 	}
 	//TODO: release resources on close
 }
+
+Server::Server() :
+	m_database(new SqliteDataBase(DATABASE_PATH)), m_communicator(m_handlerFactory), m_handlerFactory(m_database)
+{
+}
+
+Server::~Server()
+{
+	delete m_database;
+}
