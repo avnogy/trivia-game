01ee0bc6885f23248653f79bc50ded66c0f3ceaa
avner mindelis <avner.mindelis@gmail.com>
2022-05-16 14:56:50 +0000

    implemented room manager

--- Trivia Project/Backend/src/managers/RoomManager/Room.cpp
+++ Trivia Project/Backend/src/managers/RoomManager/Room.cpp
@@ -1,18 +1,23 @@
 #include "managers/RoomManager/Room.h"
 
 /// <summary>
+/// getter for room data
+/// </summary>
+/// <returns></returns>
+RoomData Room::getRoomData() const
+{
+	return m_metadata;
+}
+
+/// <summary>
 /// Adding a new user to the room
 /// </summary>
 /// <param name="user">user to add</param>
 /// <returns>whether the addition succeeded (if user already is logged in, return false)</returns>
 bool Room::addUser(const LoggedUser& user)
 {
-	//user already is logged in
-	if (m_loggedUsers.find(user) != m_loggedUsers.end())
-		return false;
-
-	m_loggedUsers.insert(user);
-	return true;
+	//insert returns true if operation was successful
+	return m_loggedUsers.insert(user).second;
 }
 
 /// <summary>
@@ -22,12 +27,8 @@ bool Room::addUser(const LoggedUser& user)
 /// <returns>whether the remove succeeded (if user already not logged in, return false)</returns>
 bool Room::removeUser(const LoggedUser& user)
 {
-	//if the user is already not is logged in
-	if (m_loggedUsers.find(user) == m_loggedUsers.end())
-		return false;
-
-	m_loggedUsers.erase(user);
-	return true;
+	//erase returns the status of the operation
+	return m_loggedUsers.erase(user);
 }
 
 /// <summary>
@@ -36,11 +37,18 @@ bool Room::removeUser(const LoggedUser& user)
 std::vector<std::string> Room::getAllUsers() const
 {
 	std::vector<std::string> allUsers;
-
 	for (const auto& user : m_loggedUsers)
 	{
 		allUsers.push_back(user.getUsername());
 	}
-
 	return allUsers;
+}
+
+Room::Room(const RoomData& data, const LoggedUser& user): m_metadata(data)
+{
+	m_loggedUsers.insert(user);
+}
+
+Room::~Room()
+{
 }
