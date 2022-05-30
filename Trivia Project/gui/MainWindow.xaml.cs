8b3c71f6ea6ff14a5cf359c121f4d15fa366af1d
Avner Mindelis <avner.mindelis@gmail.com>
2022-05-30 20:29:59 +0300

    added ui, placed project in main folder

--- Trivia Project/gui/MainWindow.xaml.cs
+++ Trivia Project/gui/MainWindow.xaml.cs
@@ -20,9 +20,16 @@ namespace gui
     /// </summary>
     public partial class MainWindow : Window
     {
+        int i = 0;
         public MainWindow()
         {
             InitializeComponent();
+            b.Content = i;
+        }
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            b.Content = i++;
         }
     }
 }
