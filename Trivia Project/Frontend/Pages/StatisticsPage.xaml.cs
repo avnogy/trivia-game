7ed613f34d3ee5439d5363b31a74687a9419dfd5
Avner Mindelis <avner.mindelis@gmail.com>
2022-06-06 15:45:31 +0300

    created statistics page

---
+++ Trivia Project/Frontend/Pages/StatisticsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frontend.Pages
+{
+    /// <summary>
+    /// Interaction logic for StatisticsPage.xaml
+    /// </summary>
+    public partial class StatisticsPage : Page
+    {
+        public StatisticsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void backBTN_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Application.Current.MainWindow).frame.Content = new MenuPage();
+        }
+
+        private void leaderboardBTN_Click(object sender, RoutedEventArgs e)
+        {
+            //((MainWindow)Application.Current.MainWindow).frame.Content = new LeaderboardPage();
+        }
+    }
+}
